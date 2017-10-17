d819d4e42f58f9ec5dd060cb89a540c74566455e
jgs1202 <jgs.kuee@gmail.com>
2017-10-17 19:17:05 +0900

    tutorial image are downloaded

--- public/write_firebase.js
+++ public/write_firebase.js
@@ -15,22 +15,22 @@ console.log("auth start")
 var provider = new firebase.auth.GoogleAuthProvider()
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
 firebase.auth().signInWithPopup(provider).then(function(result) {
-// This gives you a Google Access Token. You can use it to access the Google API.
-var token = result.credential.accessToken;
-// The signed-in user info.
-var user = result.user;
-// ...
-console.log("auth ended")
-document.getElementById("auth").style.display="block"
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+  console.log("auth ended")
+  document.getElementById("auth").style.display = "block"
 }).catch(function(error) {
-// Handle Errors here.
-var errorCode = error.code;
-var errorMessage = error.message;
-// The email of the user's account used.
-var email = error.email;
-// The firebase.auth.AuthCredential type that was used.
-var credential = error.credential;
-// ...
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
 });
 
 
@@ -57,16 +57,16 @@ function writeNewPost(username, uid, emailaddress, verified) {
 }
 //writeNewPost("01", "testuser", "pic", "Test", "This is a test.")
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function() {
 
   document.getElementById("displayAuth").addEventListener("click", viewAuth, false)
   console.log("event is set.")
-  },false)
+}, false)
 
-var viewAuth = function(e){
+var viewAuth = function(e) {
   const authData = firebase.auth().currentUser
   let divInfo = document.getElementById("userInfo")
-  divInfo.style.display="block"
+  divInfo.style.display = "block"
   let showNameJs = document.getElementById("showNameHtml")
   let showAddressJs = document.getElementById("showAddressHtml")
   let showIdJs = document.getElementById("showIdHtml")
@@ -92,12 +92,12 @@ var db = firebase.database();
 var clickWrite = function() {
   console.log("click")
   var userData = firebase.auth().currentUser
-  console.log(firebase.auth().currentUser.displayName)//.displayName, userData.uid, userData.email, userData.emailVerified)
+  console.log(firebase.auth().currentUser.displayName) //.displayName, userData.uid, userData.email, userData.emailVerified)
   let divInfo = document.getElementById("userInfo")
-  divInfo.style.display="none"
-  document.getElementById("auth").style.display="none"
-  document.getElementById("howTo").style.display="block"
-  document.getElementById("loadImageTime").style.display="block"
+  divInfo.style.display = "none"
+  document.getElementById("auth").style.display = "none"
+  document.getElementById("howTo").style.display = "block"
+  document.getElementById("loadImageTime").style.display = "block"
   writeNewPost(userData.displayName, userData.uid, userData.email, userData.emailVerified)
   downloadImg(images);
 }
@@ -105,7 +105,7 @@ var clickWrite = function() {
 window.addEventListener("load", function() {
   console.log(document.getElementById("btnChangeData"))
   document.getElementById("btnChangeData").addEventListener("click", clickWrite, false)
-  }, false)
+}, false)
 
 
 //display image file in firebase storage
@@ -117,75 +117,170 @@ let imgBlack = storageRef.child("black.png")
 //window.addEventListener("load", function(){
 //  document.getElementById("btnDisplay").addEventListener("click", downloadImg, false)
 //}, false)
-let images={}
-var downloadImg = function(images){
-  imgBlack.getDownloadURL().then(function(url){
-    images.black = new Image(500,400)
-    images.black.addEventListener("load",downloadImg2,false)
+let images = {}
+var downloadImg = function(images) {
+  imgBlack.getDownloadURL().then(function(url) {
+    images.black = new Image(500, 400)
+    images.black.addEventListener("load", downloadImg2, false)
     images.black.src = url
   })
 }
-let downloadImg2= function(){
-  for(let n=0; n<6; n++){
+let downloadImg2 = function() {
+  for (let n = 0; n < 6; n++) {
     let stringPng = ".png"
-    let imgName = ''+ n + stringPng
+    let imgName = '' + n + stringPng
     console.log(imgName)
     var imgSample = storageRef.child(imgName)
-    console.log("download start"+imgName)
-    imgSample.getDownloadURL().then(function(url){
+    console.log("download start" + imgName)
+    imgSample.getDownloadURL().then(function(url) {
       //document.getElementById("imgSample").style.backgroundImage = "url("+url+")"
-      images[n] = new Image(500,400)
-      images[n].addEventListener("load", function(){
+      images[n] = new Image(500, 400)
+      images[n].addEventListener("load", function() {
         //img.size = ImageGetNaturalSize(img)
-        if (typeof images[5] !== "undefined"){
+        if (typeof images[5] !== "undefined") {
           let imageButton = document.getElementById("imageHowTo")
-          imageButton.style.display="block"
+          imageButton.style.display = "block"
 
-          document.getElementById("loadImageTime").style.display="none"
-          imageButton.addEventListener("click", startIntervalDisplay, false)
+          document.getElementById("loadImageTime").style.display = "none"
+          imageButton.addEventListener("click",startIntervalDisplay, false)
         }
-            }, false)
+      }, false)
       images[n].src = url
       console.log("download finished.")
-    }).catch(function(error){
+    }).catch(function(error) {
       //Handle any Errors
       console.log(error)
     })
   }
 }
-window.addEventListener("load",function(){
+window.addEventListener("load", function() {
   images.place = document.getElementById("placeForImage")
   console.log(images.place)
   images.number = 0
 }, false)
-let startIntervalDisplay = function(){
+let startIntervalDisplay = function() {
   images.place.style.display = "block"
-  document.getElementById("imageHowTo").style.display="none"
+  document.getElementById("imageHowTo").style.display = "none"
   setInterval(displayImages, 500)
-  document.getElementById("explain").style.display="block"
+  document.getElementById("explain").style.display = "block"
   document.getElementById("toTutorialButton").addEventListener("click", startTutorial, false)
   console.log("event set")
-  images.time=0
+  images.time = 0
 }
-let displayImages = function(){
-  if((images.time % 3) === 0){
+let displayImages = function() {
+  if ((images.time % 3) === 0) {
     while (images.place.firstChild) images.place.removeChild(images.place.firstChild);
     images.place.appendChild(images[images.number])
     images.number += 1
-    if (images.number > 5){images.number = 0}
-  }
-  else if ((images.time % 3) === 2) {
+    if (images.number > 5) {
+      images.number = 0
+    }
+  } else if ((images.time % 3) === 2) {
     while (images.place.firstChild) images.place.removeChild(images.place.firstChild);
     images.place.appendChild(images.black)
   }
   images.time += 1
 }
 
-let startTutorial = function(e){
+let startTutorial = function(e) {
   console.log("This is tutorial.")
   document.getElementById("howTo").style.display = "none"
   document.getElementById("placeForImage").style.display = "none"
   document.getElementById("explain").style.display = "none"
   document.getElementById("tutorialFirst").style.display = "block"
-  console.log(document.getElementById("tutorialFirst"))
+  document.getElementById("startTutorialButton").addEventListener("click", TutorialEx, false)
 }
+
+let TutorialEx = function() {
+  document.getElementById("startTutorialButton").style.display = "none"
+  document.getElementById("tutorialExplain").style.display = "none"
+  document.getElementById("loadingTutorial").style.display = "block"
+  images.one = new Image(500, 400)
+  images.two = new Image(500, 400)
+  images.three = new Image(500, 400)
+  images.one.srcurl = storageRef.child("one.png")
+  images.two.srcurl = storageRef.child("second.png")
+  images.three.srcurl = storageRef.child("theree.png")
+  downloadOne()
+}
+//var storageRef = firebase.storage().ref("sampleImage")
+
+let downloadOne = function() {
+  images.one.srcurl.getDownloadURL().then(function(url) {
+    images.one.addEventListener("load", downloadTwo, false)
+    images.one.src = url
+  })
+}
+let downloadTwo = function() {
+  images.two.srcurl.getDownloadURL().then(function(url) {
+    images.two.addEventListener("load", downloadThree, false)
+    images.two.src = url
+  })
+}
+let downloadThree = function() {
+  images.three.srcurl.getDownloadURL().then(function(url) {
+    images.three.addEventListener("load", downloadImageTutorial, false)
+    images.three.src = url
+  })
+}
+let downloadImageTutorial = function() {
+  let tutorialRef = firebase.storage().ref("tutorial")
+  images.tutorial = {}
+  console.log("one, two, threee have been downloaded.")
+  for (let n = 0; n < 30; n++) {
+    let stringPng = ".png"
+    let imgName = '' + n + stringPng
+    console.log(imgName)
+    var imgTutorial = tutorialRef.child(imgName)
+    console.log("download start" + imgName)
+    imgTutorial.getDownloadURL().then(function(url) {
+      //document.getElementById("imgSample").style.backgroundImage = "url("+url+")"
+      images.tutorial[n] = new Image(500, 400)
+      images.tutorial[n].addEventListener("load", function() {
+        //img.size = ImageGetNaturalSize(img)
+        if (typeof images.tutorial[29] !== "undefined") {
+          document.getElementById("loadingTutorial").style.display = "none"
+          document.getElementById("tutorialSecond").style.display = "block"
+          //document.getElementById("loadImageTime").style.display="none"
+          document.getElementById("giveTutorialImage").addEventListener("click", startIntervalTutorial, false)
+          console.log("download finished.")
+        }
+      }, false)
+      images.tutorial[n].src = url
+    }).catch(function(error) {
+      //Handle any Errors
+      console.log(error)
+    })
+  }
+}
+let startIntervalTutorial = function() {
+  document.getElementById("giveTutorialImage").style.display = "none"
+  var timerTutorial = setInterval(displayTutorial, 500)
+  //document.getElementById("explain").style.display = "block"
+  images.time = 0
+}
+let displayTutorial = function() {
+  images.tutorial.place = document.getElementById("tutorial")
+  if(images.time === 0){
+    images.tutorial.place.appendChild(images.tutorial[images.number])
+    images.number += 1
+  }
+  if ((images.time % 3) === 0) {
+    while (images.tutorial.place.firstChild) images.tutorial.place.removeChild(images.tutorial.place.firstChild);
+    images.tutorial.place.appendChild(images.tutorial[images.number])
+    images.number += 1
+    if (images.number > 30) {
+      images.number = 0
+      clearInterval(timerTutorial)
+    }
+  } else if ((images.time % 3) === 2) {
+    while (images.tutorial.place.firstChild) images.tutorial.place.removeChild(images.tutorial.place.firstChild);
+    images.tutorial.place.appendChild(images.black)
+  }
+  images.time += 1
+}
+
+// var imgWidth = $('img#sample').width();　 //img#sampleのwidthを調べてimgWidthに代入
+// var imgHeight = $('img#sample').height();　 //img#sampleのheightを調べてimgHeightに代入
+//
+// aspectRatio = imgWidth / imgHeight　 //横幅÷縦幅の値をaspectRatioに代入
